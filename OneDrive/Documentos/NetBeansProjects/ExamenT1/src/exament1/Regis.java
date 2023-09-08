c9bb2a1b853d60e98efc43008a6ba2522c4ee00e
juan_ <juan_@DESKTOP-6F5QHQ1>
2023-09-08 13:15:46 -0500

    Primer Commit

---
+++ OneDrive/Documentos/NetBeansProjects/ExamenT1/src/exament1/Regis.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exament1;
+
+/**
+ *
+ * @author juan_
+ */
+class Regis {
+
+    Object getNombre() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
